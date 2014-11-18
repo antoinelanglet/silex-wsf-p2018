6c62ef5f05db967b43ac130a6488c3fcd6de5793
Dimitri Larue <Dimitri@Sid-portable.(none)>
2014-11-18 15:50:17 +0100

    add test logged on default

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -39,6 +39,8 @@ class UserController
             }
         }
 
+        //$app['session']->getFlashBag();
+
         //On redirige vers la home sans modifier la session
         return $app->redirect(
             $app['url_generator']->generate('home')
