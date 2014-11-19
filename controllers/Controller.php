d8c2553a69d8d193d21f1c3cbae83e5531fb31b4
Dimitri Larue <Dimitri@Sid-portable.(none)>
2014-11-19 11:53:38 +0100

    add comment

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -4,13 +4,43 @@ namespace MVC;
 class Controller 
 {
 
+    /**
+     * Les données qui seront envoyés à la vue
+     * 
+     * @var array
+     */
     protected $datas = array();
 
+    /**
+     * Inistalise le tableau datas avec les données de 
+     * l'utilisateur connecté.
+     * 
+     * @param  Silex\Application $app
+     */
     public function initAction($app)
     {
-        
+        //Recupère la variable user dans la session
+        $user = $app['session']->get('user');
+
+        //Si user n'est pas vide
+        if (!empty($user)) {
+
+            //Je met user dans le tableau datas
+            $this->datas['user'] = $user;
+
+            //Je definis la valeur idAdmin en fonction 
+            //du status admin de l'user
+            $this->datas['isAdmin'] = $user['admin'];
+        }  
     }
 
+    /**
+     * Renvoi une redirection
+     * 
+     * @param  Silex\Application $app   
+     * @param  string $route Route de destination
+     * @return Redirect
+     */
     public function redirect($app, $route)
     {
         return $app->redirect(
