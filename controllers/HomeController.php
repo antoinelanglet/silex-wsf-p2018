dc3b7a2e86d3d6124c31eb34b5604a42a45307aa
Dimitri Larue <Dimitri@Sid-portable.(none)>
2014-11-19 12:17:27 +0100

    add datas in child controller

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -8,45 +8,30 @@ class HomeController extends Controller
 {
     public function index(Request $request, Application $app)
     {
+        $this->initAction($app);
+
         //initialise le model Article
         $article = new Article();
 
-        //Recupère tous les articles
-        $all = $article->getAll();
-
-        /*//Construction de l'html
-        $html = '<html><body>';
-
-        //Boucle sur tous les articles
-        foreach ($all as $art) {
-            //html de l'article
-            $html .= '<article>'
-                  .'<h2>' . $art['title'] . '</h2>'
-                  .'<p>' . $art['body'] . '</p>'
-                  .'</article>';
-        } 
-        
-        //fin de l'html
-        $html .= '</body></html>';*/
+        //Je mets dans datas['articles'] tous mes articles
+        $this->datas['articles'] = $article->getAll();
 
         //Appel de twig, pour générer le rendu html
-        return $app['twig']->render('home/index.twig', array(
-            'articles' => $all,
-        ));
+        return $app['twig']->render('home/index.twig', $this->datas);
     }
 
     public function getArticle(Request $request, Application $app, $id)
     {
+        $this->initAction($app);
+        
         //initialise le model Article
         $article = new Article();
 
         //Recupère un article
-        $myArticle = $article->get($id);
+        $this->datas['article'] = $article->get($id);
 
         //Appel de twig, pour générer le rendu html
-        return $app['twig']->render('home/article.twig', array(
-            'article' => $myArticle,
-        ));
+        return $app['twig']->render('home/article.twig', $this->datas);
 
     }
 }
